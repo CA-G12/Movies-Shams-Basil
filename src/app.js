5d35f21e4e2baf6b155254a425d223502397687c
shamskhodary <shams.khodary@gmail.com>
2022-08-17 11:33:30 +0300

    imported the compression modules, then read static files, finally imported router relates #5

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const path = require(`path`);
 
 const express = require(`express`);
+const compression = require("compression");
+const router = require("./routes");
 const app = express();
 
 app.set(`port`, process.env.PORT || 8080);
+app.use(compression());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(router);
 
 module.exports = app;
